1bafdac2164e86681176cb6d7ba489045e4374bd
Kirit Nakka <kiritnakka@music-m1.mynetworksettings.com>
2024-03-01 16:39:07 -0500

    Finished Etch Sketch Project

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let click = 'true';
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
@@ -19,7 +20,7 @@ function populateBoard(size) {
 populateBoard(16);
 
 function changeSize(input) {
-    if (input >= 2 && input < 100) {
+    if (input >= 2 && input <= 100) {
         populateBoard(input);
     }
     else {
@@ -28,11 +29,13 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-    if (color === 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
-    else {
-        this.style.backgroundColor = color;
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+        else {
+            this.style.backgroundColor = color;
+        }
     }
 }
 
@@ -45,3 +48,15 @@ function resetBoard() {
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.style.backgroundColor = 'white');
 }
+
+document.querySelector('body').addEventListener('click', (e) =>{
+    if(e.target.tagName != 'BUTTON'){
+        click = !click;
+        if(click){
+            document.querySelector('.mode').textContent = 'Mode: Coloring';
+        }
+        else {
+            document.querySelector('.mode').textContent = "Mode: Off";
+        }
+    }
+})
